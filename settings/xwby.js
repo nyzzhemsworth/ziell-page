51502afab3b89ea635b07482f2f71fdfdab1e5c3
ziell Ackles <156542084+nyzzhemsworth@users.noreply.github.com>
2024-12-17 07:10:34 +0700

    Create xwby.js

---
+++ settings/xwby.js
@@ -0,0 +1,13 @@
+// /settings/xwby.js
+
+const profileSettings = {
+  name: "@j4vierpena", // Nama pengguna
+  description: "Welcome at my page,i hope you find what you're looking for", // Deskripsi singkat
+  profilePicture: "/media/profile.jpg", // URL foto profil
+  links: [
+    { label: "TikTok", url: "https://www.tiktok.com/j4vierpena" }, // Tambahkan link
+    { label: "Instagram", url: "https://www.instagram.com/zielll.xmll" },
+    { label: "Chihiro WhatsApp Bot", url: "https://chat.whatsapp.com/HK3aDQma8OyFHumFqNWj1Z" },
+    { label: "Aetherveil Official.", url: "https://chat.whatsapp.com/IUfCpkskDqZGxljOC1SGdv" }
+  ]
+};
