38071dc75baaec8776419b86b354ca4254f393a2
ziell Ackles <156542084+nyzzhemsworth@users.noreply.github.com>
2025-04-29 08:55:00 +0700

    Update xwby.js

--- settings/xwby.js
+++ settings/xwby.js
@@ -5,8 +5,8 @@ const profileSettings = {
   description: "Welcome at my page,i hope you find what you're looking for", // Deskripsi singkat
   profilePicture: "/media/profile.jpg", // URL foto profil
   links: [
-    { label: "TikTok", url: "https://www.tiktok.com/ziellamp" }, // Tambahkan link
-    { label: "Instagram", url: "https://www.instagram.com/ziellxmll" },
+    { label: "TikTok", url: "https://www.tiktok.com/@ziellamp?_t=ZS-8vvQQPxmbau&_r=1" }, // Tambahkan link
+    { label: "Instagram", url: "https://www.instagram.com/acklesmith?igsh=MXFpaHJpa2I4eGw5OQ==" },
     { label: "Channel Preset", url: "https://whatsapp.com/channel/0029Vb26nlxLNSZxPZjhIu2G" },
     { label: "Order Am Prem", url: "https://wa.me/6283897159514?text=hai+bang+ziell+mau+beli+am+prem" }
   ]
