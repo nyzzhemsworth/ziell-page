eade9e40b9ca4248c4723cf8b2e82f34157ea6d2
ziell Ackles <156542084+nyzzhemsworth@users.noreply.github.com>
2025-04-29 08:17:40 +0700

    Update xwby.js

--- settings/xwby.js
+++ settings/xwby.js
@@ -5,9 +5,9 @@ const profileSettings = {
   description: "Welcome at my page,i hope you find what you're looking for", // Deskripsi singkat
   profilePicture: "/media/profile.jpg", // URL foto profil
   links: [
-    { label: "TikTok", url: "https://www.tiktok.com/j4vierpena" }, // Tambahkan link
-    { label: "Instagram", url: "https://www.instagram.com/zielll.xmll" },
-    { label: "Chihiro WhatsApp Bot", url: "https://chat.whatsapp.com/HK3aDQma8OyFHumFqNWj1Z" },
-    { label: "Aetherveil Official.", url: "https://chat.whatsapp.com/IUfCpkskDqZGxljOC1SGdv" }
+    { label: "TikTok", url: "https://www.tiktok.com/ziellamp" }, // Tambahkan link
+    { label: "Instagram", url: "https://www.instagram.com/ziellxmll" },
+    { label: "Channel Preset", url: "https://whatsapp.com/channel/0029Vb26nlxLNSZxPZjhIu2G" },
+    { label: "Order Am Prem", url: "https://wa.me/6283897159514?text=hai+bang+ziell+mau+beli+am+prem" }
   ]
 };
